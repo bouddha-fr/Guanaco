18852b269621d643be791808047af8c1afb5f91e
bouddha-fr <bouddha.whitehat@gmail.com>
2024-03-29 13:28:45 +0100

    1.1

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 ![Guanaco](https://i.imgur.com/KvccnAT.png)
 
-## Langages utilisés ?
-
-- Python
-
-## Utilités
-
-- Remonte les informations sur l'état du CPU, de la RAM ainsi que de l'espace de stockage.
+**Remonte les informations sur l'état du CPU, de la RAM ainsi que de l'espace de stockage.
 
 ## Screenshots
 
--- guanaco.py
+++ guanaco.py
@@ -8,6 +8,7 @@ bot = commands.Bot(command_prefix='g!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} est connecté.')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="g!cbum"))
     stats.start()
 
 #Permet d'aller chercher l'@publique
@@ -15,14 +16,13 @@ try:
    response = requests.get('https://api.ipify.org/?format=json')
    public_ip = response.json()['ip']
 except Exception as e:
-   print(f"Erreur lors de la récupération de l'adresse IP publique : {e}")
    public_ip = 'N/A'
 
 @bot.command()
 async def cbum(ctx):
-    photo_url = "https://pbs.twimg.com/media/FkCfB1AXwAAO1ae.jpg"
+    photo_cbum = "https://pbs.twimg.com/media/FkCfB1AXwAAO1ae.jpg"
     bedem = discord.Embed(title='Chris Bumstead', description='Tema la moustache #moustache #idolemoustache', color=0xff4c4c)
-    bedem.set_image(url=photo_url)
+    bedem.set_image(url=photo_cbum)
     await ctx.send(embed=bedem)
 
 @bot.command()
@@ -32,18 +32,23 @@ async def disk(ctx):
         partition_info = psutil.disk_usage(partition.mountpoint)
         bedem = discord.Embed(title='Disque', description=f'Espace disque utilisé pour {partition.mountpoint}', color=0xffad31)
         bedem.add_field(name='Total', value=f'{partition_info.total / (1024*1024*1024):.2f} GB', inline=False)
-        bedem.add_field(name='Used', value=f'{partition_info.used / (1024*1024*1024):.2f} GB', inline=False)
+        bedem.add_field(name='Utilisé', value=f'{partition_info.used / (1024*1024*1024):.2f} GB', inline=False)
         bedem.add_field(name='Free', value=f'{partition_info.free / (1024*1024*1024):.2f} GB', inline=False)
         await ctx.send(embed=bedem)
 
 @tasks.loop(hours=1)
 async def stats():
     channel = bot.get_channel(1220667939619864578)
+    photo_raspi = "https://i.imgur.com/xnByQbA.png"
+    used_gb = psutil.virtual_memory().used / (1024**3)
+    available_gb = psutil.virtual_memory().available / (1024**3)
+    total_gb = psutil.virtual_memory().total / (1024**3)
     bedem = discord.Embed(title='Rasberry Pi 4 > Infra Maison', color=0x7289DA)
+    bedem.set_thumbnail(url=photo_raspi)
     bedem.add_field(name='IPv4', value=public_ip, inline=False)
     bedem.add_field(name='Utilisation CPU', value=f'{psutil.cpu_percent()}%', inline=False)
-    bedem.add_field(name='Utilisation RAM', value=f'{psutil.virtual_memory().percent}%', inline=False)
-    bedem.add_field(name='RAM disponible', value=f'{psutil.virtual_memory().available * 100 / psutil.virtual_memory().total}%', inline=False)
+    bedem.add_field(name='Utilisation RAM', value=f'{used_gb:.1f} GB / {total_gb:.1f} GB', inline=False)
+    bedem.add_field(name='RAM disponible', value=f'{available_gb:.1f} GB / {total_gb:.1f} GB', inline=False)
     await channel.send(embed=bedem)
 
 with open("token.txt", "r") as file:
